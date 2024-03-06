3bfc2819eb5d82fe132e8a5ebe439a0e614f8247
Shikha Maheshwari <shikhamaheshwari@Shikhas-MacBook-Pro.local>
2024-03-06 17:26:40 +0530

    fix: update variable

--- tests/resources/variables.tf
+++ tests/resources/variables.tf
@@ -13,7 +13,7 @@ variable "region" {
 variable "prefix" {
   type        = string
   description = "Prefix to append to all resources created by this example."
-  default     = "obs-da-4"
+  default     = "obs-da-5"
 }
 
 variable "resource_tags" {
