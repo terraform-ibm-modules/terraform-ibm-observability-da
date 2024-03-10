74b147175331f58b9e8ec379b12f692af2dcc7d0
Shikha Maheshwari <shikhamaheshwari@Shikhas-MacBook-Pro.local>
2024-03-10 18:00:37 +0530

    fix: update prefix

--- tests/resources/variables.tf
+++ tests/resources/variables.tf
@@ -13,7 +13,7 @@ variable "region" {
 variable "prefix" {
   type        = string
   description = "Prefix to append to all resources created by this example."
-  default     = "test-agents-da"
+  default     = "agents-da"
 }
 
 variable "resource_tags" {
