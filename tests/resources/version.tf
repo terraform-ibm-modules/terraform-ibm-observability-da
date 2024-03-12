4481e4ea699808f04573bb8e94cfa14fdea578d6
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2024-03-12 10:30:20 +0530

    test

--- tests/resources/version.tf
+++ tests/resources/version.tf
@@ -14,10 +14,12 @@ terraform {
       source  = "hashicorp/external"
       version = ">= 2.3.2"
     }
+    # tflint-ignore: terraform_unused_required_providers
     random = {
       source  = "hashicorp/random"
       version = ">= 3.6.0"
     }
+    # tflint-ignore: terraform_unused_required_providers
     time = {
       source  = "hashicorp/time"
       version = ">= 0.11.1"
