73d874a8959e274483bc9823086556b2c0087fcf
Md Anam Raihan <Md.Anam.Raihan@ibm.com>
2024-03-13 04:26:39 +0000

    fix: add test SKIP UPGRADE TEST

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -10,14 +10,15 @@ import (
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/cloudinfo"
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/common"
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testhelper"
+	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testschematic"
 )
 
 const yamlLocation = "../common-dev-assets/common-go-assets/common-permanent-resources.yaml"
 const resourceGroup = "geretain-test-observability-instances"
-const solutionInstanceTerraformDir = "solutions/instances"
-const region = "us-south"
+const solutionInstanceDADir = "solutions/instances"
 
 var sharedInfoSvc *cloudinfo.CloudInfoService
+
 var permanentResources map[string]interface{}
 
 func TestMain(m *testing.M) {
@@ -33,24 +34,65 @@ func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
 
-func TestRunInstanceSolution(t *testing.T) {
+func TestInstancesInSchematics(t *testing.T) {
+	t.Parallel()
+
+	const region = "us-south"
+
+	options := testschematic.TestSchematicOptionsDefault(&testschematic.TestSchematicOptions{
+		Testing: t,
+		Prefix:  "instance-da",
+		TarIncludePatterns: []string{
+			"*.tf",
+			solutionInstanceDADir + "/*.tf",
+		},
+		ResourceGroup:          resourceGroup,
+		TemplateFolder:         solutionInstanceDADir,
+		Tags:                   []string{"test-schematic"},
+		DeleteWorkspaceOnFail:  false,
+		WaitJobCompleteMinutes: 60,
+		Region:                 region,
+	})
+
+	options.TerraformVars = []testschematic.TestSchematicTerraformVar{
+		{Name: "ibmcloud_api_key", Value: options.RequiredEnvironmentVars["TF_VAR_ibmcloud_api_key"], DataType: "string", Secure: true},
+		{Name: "resource_group_name", Value: options.Prefix, DataType: "string"},
+		{Name: "existing_kms_guid", Value: permanentResources["hpcs_south"], DataType: "string"},
+		{Name: "kms_region", Value: "us-south", DataType: "string"}, // KMS instance is in us-south
+		{Name: "cos_region", Value: region, DataType: "string"},
+		{Name: "cos_instance_tags", Value: options.Tags, DataType: "list(string)"},
+		{Name: "log_analysis_tags", Value: options.Tags, DataType: "list(string)"},
+		{Name: "cloud_monitoring_tags", Value: options.Tags, DataType: "list(string)"},
+		{Name: "cos_instance_access_tags", Value: permanentResources["accessTags"], DataType: "list(string)"},
+		{Name: "cos_bucket_access_tags", Value: permanentResources["accessTags"], DataType: "list(string)"},
+	}
+
+	err := options.RunSchematicTest()
+	assert.Nil(t, err, "This should not have errored")
+}
+
+func TestRunUpgradeInstances(t *testing.T) {
 	t.Parallel()
 
 	options := testhelper.TestOptionsDefault(&testhelper.TestOptions{
-		Testing:       t,
-		TerraformDir:  solutionInstanceTerraformDir,
-		Region:        region,
-		ResourceGroup: resourceGroup,
+		Testing:      t,
+		TerraformDir: solutionInstanceDADir,
+		Region:       "us-south",
+		Prefix:       "obs-ins-upg",
 	})
 
 	options.TerraformVars = map[string]interface{}{
-		"cos_instance_access_tags": permanentResources["accessTags"],
-		"existing_kms_guid":        permanentResources["hpcs_south"],
-		"existing_resource_group":  true,
-		"resource_group_name":      options.ResourceGroup,
+		"resource_group_name":                 options.Prefix,
+		"cos_instance_access_tags":            permanentResources["accessTags"],
+		"existing_kms_guid":                   permanentResources["hpcs_south"],
+		"kms_endpoint_type":                   "public",
+		"kms_region":                          "us-south",
+		"management_endpoint_type_for_bucket": "public",
+		"log_analysis_service_endpoints":      "public-and-private",
+		"cloud_monitoring_service_endpoints":  "public",
 	}
 
-	output, err := options.RunTestConsistency()
+	output, err := options.RunTestUpgrade()
 	assert.Nil(t, err, "This should not have errored")
 	assert.NotNil(t, output, "Expected some output")
 }
