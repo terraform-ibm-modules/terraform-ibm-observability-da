fcfe0aeb960bd426cfd7186fcd74aa4971447d03
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-04-13 18:00:58 +0000

    fix(deps): update required_provider to latest for the deployable architecture solution

--- solutions/agents/version.tf
+++ solutions/agents/version.tf
@@ -6,7 +6,7 @@ terraform {
     # Lock DA into an exact provider version - renovate automation will keep it updated
     ibm = {
       source  = "ibm-cloud/ibm"
-      version = "1.64.0"
+      version = "1.64.1"
     }
     helm = {
       source  = "hashicorp/helm"
@@ -14,7 +14,7 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.27.0"
+      version = "2.29.0"
     }
   }
 }
