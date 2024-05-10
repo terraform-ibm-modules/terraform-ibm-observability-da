835dd4332f932d7f44b607d2cdd44dee50cd4297
Mohammad Soaib <mohammad.soaib@ibm.com>
2024-05-10 17:28:10 +0530

    update variable description

--- solutions/instances/variables.tf
+++ solutions/instances/variables.tf
@@ -97,7 +97,7 @@ variable "enable_platform_logs" {
 
 variable "enable_at_event_routing_to_cos_bucket" {
   type        = bool
-  description = "Set to true to enable activity tracker event routing to the provisioned Cloud Object Storage bucket."
+  description = "Set to true to enable activity tracker event routing to the Cloud Object Storage (COS) bucket."
   default     = true
 }
 
