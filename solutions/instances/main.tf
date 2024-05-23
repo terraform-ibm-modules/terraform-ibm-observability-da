7d16e7488c43732de5c4af869f8c5b609bc72a1f
Aayush-Abhyarthi <122350533+Aayush-Abhyarthi@users.noreply.github.com>
2024-05-23 12:53:16 +0530

    feat: provided the ability to opt out of provisioning logging or monitoring instances using new boolean input variables `log_analysis_provision` and `cloud_monitoring_provision` (#45)

--- solutions/instances/main.tf
+++ solutions/instances/main.tf
@@ -3,6 +3,10 @@
 #######################################################################################################################
 
 locals {
+
+  # tflint-ignore: terraform_unused_declarations
+  validate_log_analysis_provision = var.enable_at_event_routing_to_log_analysis && var.log_analysis_provision == false ? tobool("log_analysis_provision can't be false if enable_at_event_routing_to_log_analysis is true") : true
+
   archive_api_key    = var.log_archive_api_key == null ? var.ibmcloud_api_key : var.log_archive_api_key
   default_cos_region = var.cos_region != null ? var.cos_region : var.region
 
@@ -21,13 +25,13 @@ locals {
   cos_target_bucket_name     = var.existing_at_cos_target_bucket_name != null ? var.existing_at_cos_target_bucket_name : module.cos_bucket[0].buckets[local.at_cos_target_bucket_name].bucket_name
   cos_target_bucket_endpoint = var.existing_at_cos_target_bucket_endpoint != null ? var.existing_at_cos_target_bucket_endpoint : module.cos_bucket[0].buckets[local.at_cos_target_bucket_name].s3_endpoint_private
 
-  bucket_config_1 = var.existing_log_archive_cos_bucket_name == null ? {
+  bucket_config_1 = var.existing_log_archive_cos_bucket_name == null && var.log_analysis_provision == true ? {
     class = var.log_archive_cos_bucket_class
     name  = local.log_archive_cos_bucket_name
     tag   = var.archive_bucket_access_tags
   } : null
 
-  bucket_config_2 = var.existing_at_cos_target_bucket_name == null ? {
+  bucket_config_2 = var.existing_at_cos_target_bucket_name == null && var.enable_at_event_routing_to_cos_bucket == true ? {
     class = var.at_cos_target_bucket_class
     name  = local.at_cos_target_bucket_name
     tag   = var.at_cos_bucket_access_tags
@@ -100,7 +104,7 @@ module "observability_instance" {
   enable_archive    = var.enable_log_archive
   ibmcloud_api_key  = local.archive_api_key
   # Log Analysis
-  log_analysis_provision           = true
+  log_analysis_provision           = var.log_analysis_provision
   log_analysis_instance_name       = var.prefix != null ? "${var.prefix}-${var.log_analysis_instance_name}" : var.log_analysis_instance_name
   log_analysis_plan                = var.log_analysis_plan
   log_analysis_tags                = var.log_analysis_tags
@@ -110,7 +114,7 @@ module "observability_instance" {
   log_analysis_cos_bucket_endpoint = local.archive_cos_bucket_endpoint
   enable_platform_logs             = var.enable_platform_logs
   # IBM Cloud Monitoring
-  cloud_monitoring_provision         = true
+  cloud_monitoring_provision         = var.cloud_monitoring_provision
   cloud_monitoring_instance_name     = var.prefix != null ? "${var.prefix}-${var.cloud_monitoring_instance_name}" : var.cloud_monitoring_instance_name
   cloud_monitoring_plan              = var.cloud_monitoring_plan
   cloud_monitoring_tags              = var.cloud_monitoring_tags
@@ -207,7 +211,7 @@ module "cos_instance" {
   providers = {
     ibm = ibm.cos
   }
-  count                    = (var.existing_cos_instance_crn == null) ? 1 : 0 # no need to call COS module if consumer is using existing COS instance
+  count                    = (var.existing_cos_instance_crn == null) && length(local.bucket_config_map) != 0 ? 1 : 0 # no need to call COS module if consumer is using existing COS instance
   source                   = "terraform-ibm-modules/cos/ibm//modules/fscloud"
   version                  = "7.5.3"
   resource_group_id        = module.resource_group.resource_group_id
