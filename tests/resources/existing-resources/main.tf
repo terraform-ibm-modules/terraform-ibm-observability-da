069031c8cac992b9a239b7f09dda3b1aa03c58dc
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-07-13 11:07:17 -0400

    chore(deps): update terraform-module (#121)

--- tests/resources/existing-resources/main.tf
+++ tests/resources/existing-resources/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "cos" {
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "8.5.3"
+  version                = "8.6.2"
   resource_group_id      = module.resource_group.resource_group_id
   region                 = var.region
   cos_instance_name      = "${var.prefix}-cos"
@@ -28,7 +28,7 @@ module "cos" {
 
 module "additional_cos_bucket" {
   source                   = "terraform-ibm-modules/cos/ibm"
-  version                  = "8.5.3"
+  version                  = "8.6.2"
   region                   = var.region
   create_cos_instance      = false
   existing_cos_instance_id = module.cos.cos_instance_id
