5efd019df2d7e646e1b62883da8e7a28b0fdeeb5
dishankkalra23 <dishankalra@gmail.com>
2024-09-26 13:13:54 +0530

    fix: add suport for wait_till cluster is ready

--- solutions/agents/provider.tf
+++ solutions/agents/provider.tf
@@ -13,3 +13,19 @@ provider "helm" {
     token = data.ibm_container_cluster_config.cluster_config.token
   }
 }
+
+# Retrieve information about an existing VPC cluster
+data "ibm_container_vpc_cluster" "cluster" {
+  count             = var.is_vpc_cluster ? 1 : 0
+  name              = var.cluster_id
+  wait_till         = var.wait_till
+  wait_till_timeout = var.wait_till_timeout
+}
+
+# Retrieve information about an existing Classic cluster
+data "ibm_container_cluster" "cluster" {
+  count             = var.is_vpc_cluster ? 0 : 1
+  name              = var.cluster_id
+  wait_till         = var.wait_till
+  wait_till_timeout = var.wait_till_timeout
+}
