4a3a47f0d04810be97355c4e789eaea02dcecf52
Md Anam Raihan <md.anam.raihan17@gmail.com>
2024-10-04 23:17:04 +0530

    feat: supported added to the `agents` variation to provision the logs agent for sending logs to IBM Cloud logs<br>- NOTE: The Log Analysis agent is not longer deploted by default due to the Log Analysis services being deprecated (#167)

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -1,4 +1,16 @@
 ##############################################################################
+# Resource Group
+##############################################################################
+
+module "resource_group" {
+  source  = "terraform-ibm-modules/resource-group/ibm"
+  version = "1.1.6"
+  # if an existing resource group is not set (null) create a new one using prefix
+  resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
+  existing_resource_group_name = var.resource_group
+}
+
+##############################################################################
 # SLZ ROKS Pattern
 ##############################################################################
 
@@ -16,28 +28,117 @@ module "landing_zone" {
 }
 
 ##############################################################################
-# Observability Instances
+# COS instance
+##############################################################################
+
+module "cos" {
+  source            = "terraform-ibm-modules/cos/ibm"
+  version           = "8.11.14"
+  resource_group_id = module.resource_group.resource_group_id
+  cos_instance_name = "${var.prefix}-cos"
+  cos_tags          = var.resource_tags
+  create_cos_bucket = false
+}
+
+##############################################################################
+# COS buckets
+##############################################################################
+
+locals {
+  logs_bucket_name    = "${var.prefix}-logs-data"
+  metrics_bucket_name = "${var.prefix}-metrics-data"
+}
+
+module "buckets" {
+  source  = "terraform-ibm-modules/cos/ibm//modules/buckets"
+  version = "8.11.14"
+  bucket_configs = [
+    {
+      bucket_name            = local.logs_bucket_name
+      kms_encryption_enabled = false
+      region_location        = var.region
+      resource_instance_id   = module.cos.cos_instance_id
+    },
+    {
+      bucket_name            = local.metrics_bucket_name
+      kms_encryption_enabled = false
+      region_location        = var.region
+      resource_instance_id   = module.cos.cos_instance_id
+    }
+  ]
+}
+
+##############################################################################
+# Observability:
+# - Cloud Logs instance
+# - Monitoring instance
 ##############################################################################
 
 locals {
   cluster_resource_group_id = module.landing_zone.cluster_data["${var.prefix}-workload-cluster"].resource_group_id
+  cluster_crn               = module.landing_zone.cluster_data["${var.prefix}-workload-cluster"].crn
 }
 
 module "observability_instances" {
-  source = "git::https://github.com/terraform-ibm-modules/terraform-ibm-observability-instances?ref=v2.18.0"
+  source  = "terraform-ibm-modules/observability-instances/ibm"
+  version = "2.19.1"
   providers = {
     logdna.at = logdna.at
     logdna.ld = logdna.ld
   }
   resource_group_id                  = local.cluster_resource_group_id
   region                             = var.region
-  log_analysis_plan                  = "7-day"
-  log_analysis_service_endpoints     = "public-and-private"
-  log_analysis_instance_name         = "${var.prefix}-log-analysis"
-  enable_platform_logs               = false
   cloud_monitoring_plan              = "graduated-tier"
   cloud_monitoring_service_endpoints = "public-and-private"
   cloud_monitoring_instance_name     = "${var.prefix}-cloud-monitoring"
+  cloud_logs_instance_name           = "${var.prefix}-cloud-logs"
   enable_platform_metrics            = false
-  activity_tracker_provision         = false
+  enable_platform_logs               = false
+  cloud_logs_tags                    = var.resource_tags
+  cloud_logs_data_storage = {
+    # logs and metrics buckets must be different
+    logs_data = {
+      enabled         = true
+      bucket_crn      = module.buckets.buckets[local.logs_bucket_name].bucket_crn
+      bucket_endpoint = module.buckets.buckets[local.logs_bucket_name].s3_endpoint_direct
+    },
+    metrics_data = {
+      enabled         = true
+      bucket_crn      = module.buckets.buckets[local.metrics_bucket_name].bucket_crn
+      bucket_endpoint = module.buckets.buckets[local.metrics_bucket_name].s3_endpoint_direct
+    }
+  }
+}
+
+##############################################################################
+# Trusted Profile
+##############################################################################
+
+locals {
+  logs_agent_namespace = "ibm-observe"
+  logs_agent_name      = "logs-agent"
+}
+
+module "trusted_profile" {
+  source                      = "terraform-ibm-modules/trusted-profile/ibm"
+  version                     = "1.0.4"
+  trusted_profile_name        = "${var.prefix}-profile"
+  trusted_profile_description = "Logs agent Trusted Profile"
+  # As a `Sender`, you can send logs to your IBM Cloud Logs service instance - but not query or tail logs. This role is meant to be used by agents and routers sending logs.
+  trusted_profile_policies = [{
+    roles = ["Sender"]
+    resources = [{
+      service = "logs"
+    }]
+  }]
+  # Set up fine-grained authorization for `logs-agent` running in ROKS cluster in `ibm-observe` namespace.
+  trusted_profile_links = [{
+    cr_type = "ROKS_SA"
+    links = [{
+      crn       = local.cluster_crn
+      namespace = local.logs_agent_namespace
+      name      = local.logs_agent_name
+    }]
+    }
+  ]
 }
