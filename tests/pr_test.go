a93cf3a0ad659d07ec5a3a634906fe83480e4680
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2024-10-22 08:38:28 +0100

    test: re-enable multiple EN integrations in test

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -268,13 +268,12 @@ func TestRunExistingResourcesInstances(t *testing.T) {
 				"existing_cloud_logs_metrics_bucket_crn":      terraform.Output(t, existingTerraformOptions, "metrics_bucket_crn"),
 				"existing_cloud_logs_metrics_bucket_endpoint": terraform.Output(t, existingTerraformOptions, "metrics_bucket_endpoint"),
 				"existing_en_instance_crn":                    terraform.Output(t, existingTerraformOptions, "en_crn_1"),
-				// temporarily disabled until https://github.ibm.com/GoldenEye/issues/issues/11159 is resolved
-				// "cloud_logs_existing_en_instances": []map[string]interface{}{
-				// 	{
-				// 		"instance_crn":     terraform.Output(t, existingTerraformOptions, "en_crn_2"),
-				// 		"integration_name": "en-2",
-				// 	},
-				// },
+				"cloud_logs_existing_en_instances": []map[string]interface{}{
+					{
+						"instance_crn":     terraform.Output(t, existingTerraformOptions, "en_crn_2"),
+						"integration_name": "en-2",
+					},
+				},
 				"management_endpoint_type_for_bucket": "public",
 				"enable_platform_metrics":             "false",
 				"enable_platform_logs":                "false",
