30b17c4a8606c65fc254920633f234f794683163
Md Anam Raihan <md.anam.raihan@ibm.com>
2024-11-04 21:08:15 +0530

    fix: add iam_auth_policy variable

--- solutions/instances/main.tf
+++ solutions/instances/main.tf
@@ -289,6 +289,7 @@ module "observability_instance" {
       destination_crn = module.observability_instance.cloud_monitoring_crn
       target_name     = local.metric_router_target_name
       target_region   = var.region
+      skip_mrouter_sysdig_iam_auth_policy = false
     }
   ] : []
 
