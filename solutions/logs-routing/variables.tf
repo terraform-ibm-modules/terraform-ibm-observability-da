03b05654b49c0d30e08e7182c677bfcbedd84273
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2025-02-17 11:58:24 +0000

    Update variables.tf

--- solutions/logs-routing/variables.tf
+++ solutions/logs-routing/variables.tf
@@ -7,8 +7,7 @@ variable "ibmcloud_api_key" {
 variable "provider_visibility" {
   description = "Set the visibility value for the IBM terraform provider. Supported values are `public`, `private`, `public-and-private`. [Learn more](https://registry.terraform.io/providers/IBM-Cloud/ibm/latest/docs/guides/custom-service-endpoints)."
   type        = string
-  default     = "public"
-  # NOTE. Provider visibility is set to public because of provider issue. (https://github.com/IBM-Cloud/terraform-provider-ibm/issues/5977)
+  default     = "private"
 
   validation {
     condition     = contains(["public", "private", "public-and-private"], var.provider_visibility)
