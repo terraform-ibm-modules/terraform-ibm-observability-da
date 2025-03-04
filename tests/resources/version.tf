a07a50c67b25ec9e9e58093bbce9a05402cd5968
ocofaigh <ocofaigh@ie.ibm.com>
2025-03-04 16:25:05 +0000

    format

--- tests/resources/version.tf
+++ tests/resources/version.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.0.0"
   required_providers {
     ibm = {
-      source  = "ibm-cloud/ibm"
+      source = "ibm-cloud/ibm"
       # lock into 1.75.2 until fix for https://github.com/IBM-Cloud/terraform-provider-ibm/issues/6036 is released
       version = "1.75.2"
     }
