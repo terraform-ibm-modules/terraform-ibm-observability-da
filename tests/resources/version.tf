5b1db653d40aaebfdaff3dbafaa83dee449e2321
Akash Kumar <akash.kumar@ibm.com>
2025-03-06 18:16:12 +0530

    chore: bump provider version

--- tests/resources/version.tf
+++ tests/resources/version.tf
@@ -3,8 +3,8 @@ terraform {
   required_providers {
     ibm = {
       source = "ibm-cloud/ibm"
-      # lock into 1.75.2 until fix for https://github.com/IBM-Cloud/terraform-provider-ibm/issues/6036 is released
-      version = "1.75.2"
+      # lock into 1.76.0 until fix for https://github.com/IBM-Cloud/terraform-provider-ibm/issues/6036 is released
+      version = "1.76.0"
     }
   }
 }
