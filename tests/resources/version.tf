2d764797521638933475fa49912e37fe85b48796
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2025-03-11 10:04:34 +0000

    Update version.tf

--- tests/resources/version.tf
+++ tests/resources/version.tf
@@ -2,8 +2,7 @@ terraform {
   required_version = ">= 1.0.0"
   required_providers {
     ibm = {
-      source = "ibm-cloud/ibm"
-      # lock into 1.75.2 until fix for https://github.com/IBM-Cloud/terraform-provider-ibm/issues/6036 is released
+      source  = "ibm-cloud/ibm"
       version = "1.75.2"
     }
   }
