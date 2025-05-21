fd45075d098e1bfd3e27b5ae4bf5f2cf896ee9be
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-05-21 11:20:36 +0000

    fix(deps): update terraform terraform-ibm-modules/kms-all-inclusive/ibm to v5.1.6

--- solutions/instances/main.tf
+++ solutions/instances/main.tf
@@ -384,7 +384,7 @@ module "kms" {
   }
   count                       = (var.existing_cos_kms_key_crn != null || (length(coalesce(local.buckets_config, [])) == 0)) ? 0 : 1 # no need to create any KMS resources if passing an existing key, or bucket
   source                      = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version                     = "5.1.5"
+  version                     = "5.1.6"
   create_key_protect_instance = false
   region                      = local.kms_region
   existing_kms_instance_crn   = var.existing_kms_instance_crn
