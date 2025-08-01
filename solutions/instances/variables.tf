e70b6874e05ec4b3b287ef0437714d068729db7b
whoffler <WHoffler@ie.ibm.com>
2025-08-01 13:35:36 +0100

    update description

--- solutions/instances/variables.tf
+++ solutions/instances/variables.tf
@@ -30,7 +30,7 @@ variable "use_existing_resource_group" {
 
 variable "resource_group_name" {
   type        = string
-  description = "The name of the resource group to provision resources in."
+  description = "The name of an existing resource group to provision the resources. If not provided the default resource group will be used."
   default     = null
 }
 
