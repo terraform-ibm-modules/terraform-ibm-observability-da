20a1d3d7a5d77754e4c6c72cb22663252c184a0b
Md Anam Raihan <md.anam.raihan17@gmail.com>
2025-08-11 12:24:01 +0000

    update base-ocp-vpc version

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -180,7 +180,7 @@ locals {
 
 module "ocp_base" {
   source               = "terraform-ibm-modules/base-ocp-vpc/ibm"
-  version              = "3.54.7"
+  version              = "3.55.1"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
   tags                 = var.resource_tags
