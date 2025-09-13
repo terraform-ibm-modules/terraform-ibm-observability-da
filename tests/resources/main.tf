16f11b784c5d24bae097c842e13dfb9e31e094dd
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-09-13 04:14:22 +0100

    fix(deps): update terraform terraform-ibm-modules/cloud-logs/ibm to v1.6.25 (#475)

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -96,7 +96,7 @@ module "cloud_monitoring" {
 
 module "cloud_logs" {
   source            = "terraform-ibm-modules/cloud-logs/ibm"
-  version           = "1.6.24"
+  version           = "1.6.25"
   region            = var.region
   resource_group_id = module.resource_group.resource_group_id
   instance_name     = var.prefix
