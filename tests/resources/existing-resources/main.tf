92c5ba5399ca145f2e55c925626f0bfee21b9f95
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-01-03 04:18:51 +0000

    fix(deps): update dependencies (#556)

--- tests/resources/existing-resources/main.tf
+++ tests/resources/existing-resources/main.tf
@@ -62,7 +62,7 @@ module "cloud_log_buckets" {
 
 module "cloud_monitoring" {
   source                  = "terraform-ibm-modules/cloud-monitoring/ibm"
-  version                 = "1.12.13"
+  version                 = "1.12.14"
   region                  = var.region
   resource_group_id       = module.resource_group.resource_group_id
   instance_name           = var.prefix
