71fedcc8c909399306f9a82d40cb7ab55be4e3bc
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-01-13 00:13:37 +0000

    fix(deps): update dependencies (#573)

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -96,7 +96,7 @@ module "cloud_monitoring" {
 
 module "cloud_logs" {
   source            = "terraform-ibm-modules/cloud-logs/ibm"
-  version           = "1.10.21"
+  version           = "1.10.22"
   region            = var.region
   resource_group_id = module.resource_group.resource_group_id
   instance_name     = var.prefix
