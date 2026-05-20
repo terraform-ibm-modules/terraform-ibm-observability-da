fcd9d7733bc6417168f9ad5bd5e127788ff0510d
mukul-palit <mukul.palit@ibm.com>
2026-05-20 11:18:43 +0530

    fix(deps): deprecated terratest functions and add context

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -2,6 +2,7 @@
 package test
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
@@ -162,9 +163,9 @@ func TestAgentsSolutionInSchematics(t *testing.T) {
 	// Deploy SLZ ROKS Cluster and Observability instances since it is needed to deploy Observability Agents
 	// ------------------------------------------------------------------------------------------------------
 
-	prefix := fmt.Sprintf("slz-%s", strings.ToLower(random.UniqueId()))
+	prefix := fmt.Sprintf("slz-%s", strings.ToLower(random.UniqueID()))
 	realTerraformDir := "./resources"
-	tempTerraformDir, _ := files.CopyTerraformFolderToTemp(realTerraformDir, fmt.Sprintf(prefix+"-%s", strings.ToLower(random.UniqueId())))
+	tempTerraformDir, _ := files.CopyTerraformFolderToTemp(realTerraformDir, fmt.Sprintf(prefix+"-%s", strings.ToLower(random.UniqueID())))
 
 	// Verify ibmcloud_api_key variable is set
 	checkVariable := "TF_VAR_ibmcloud_api_key"
@@ -184,8 +185,8 @@ func TestAgentsSolutionInSchematics(t *testing.T) {
 		Upgrade: true,
 	})
 
-	terraform.WorkspaceSelectOrNew(t, existingTerraformOptions, prefix)
-	_, existErr := terraform.InitAndApplyE(t, existingTerraformOptions)
+	terraform.WorkspaceSelectOrNewContext(t, context.Background(), existingTerraformOptions, prefix)
+	_, existErr := terraform.InitAndApplyContextE(t, context.Background(), existingTerraformOptions)
 
 	if existErr != nil {
 		assert.True(t, existErr == nil, "Init and Apply of temp resources (SLZ-ROKS and Observability Instances) failed")
@@ -215,11 +216,11 @@ func TestAgentsSolutionInSchematics(t *testing.T) {
 		options.TerraformVars = []testschematic.TestSchematicTerraformVar{
 			{Name: "ibmcloud_api_key", Value: options.RequiredEnvironmentVars["TF_VAR_ibmcloud_api_key"], DataType: "string", Secure: true},
 			{Name: "cloud_monitoring_instance_region", Value: region, DataType: "string"},
-			{Name: "cluster_id", Value: terraform.Output(t, existingTerraformOptions, "cluster_id"), DataType: "string"},
-			{Name: "logs_agent_trusted_profile", Value: terraform.Output(t, existingTerraformOptions, "trusted_profile_id"), DataType: "string"},
-			{Name: "cloud_logs_ingress_endpoint", Value: terraform.Output(t, existingTerraformOptions, "cloud_logs_ingress_private_endpoint"), DataType: "string"},
-			{Name: "cluster_resource_group_id", Value: terraform.Output(t, existingTerraformOptions, "resource_group_id"), DataType: "string"},
-			{Name: "cloud_monitoring_access_key", Value: terraform.Output(t, existingTerraformOptions, "cloud_monitoring_access_key"), DataType: "string", Secure: true},
+			{Name: "cluster_id", Value: terraform.OutputContext(t, context.Background(), existingTerraformOptions, "cluster_id"), DataType: "string"},
+			{Name: "logs_agent_trusted_profile", Value: terraform.OutputContext(t, context.Background(), existingTerraformOptions, "trusted_profile_id"), DataType: "string"},
+			{Name: "cloud_logs_ingress_endpoint", Value: terraform.OutputContext(t, context.Background(), existingTerraformOptions, "cloud_logs_ingress_private_endpoint"), DataType: "string"},
+			{Name: "cluster_resource_group_id", Value: terraform.OutputContext(t, context.Background(), existingTerraformOptions, "resource_group_id"), DataType: "string"},
+			{Name: "cloud_monitoring_access_key", Value: terraform.OutputContext(t, context.Background(), existingTerraformOptions, "cloud_monitoring_access_key"), DataType: "string", Secure: true},
 			{Name: "prefix", Value: options.Prefix, DataType: "string"},
 		}
 
@@ -234,8 +235,8 @@ func TestAgentsSolutionInSchematics(t *testing.T) {
 		fmt.Println("Terratest failed. Debug the test and delete resources manually.")
 	} else {
 		logger.Log(t, "START: Destroy (existing resources)")
-		terraform.Destroy(t, existingTerraformOptions)
-		terraform.WorkspaceDelete(t, existingTerraformOptions, prefix)
+		terraform.DestroyContext(t, context.Background(), existingTerraformOptions)
+		terraform.WorkspaceDeleteContext(t, context.Background(), existingTerraformOptions, prefix)
 		logger.Log(t, "END: Destroy (existing resources)")
 	}
 }
@@ -247,9 +248,9 @@ func TestRunExistingResourcesInstancesSchematics(t *testing.T) {
 	// Provision COS & EN first
 	// ------------------------------------------------------------------------------------
 
-	prefix := fmt.Sprintf("obs-exist-%s", strings.ToLower(random.UniqueId()))
+	prefix := fmt.Sprintf("obs-exist-%s", strings.ToLower(random.UniqueID()))
 	realTerraformDir := "./resources/existing-resources"
-	tempTerraformDir, _ := files.CopyTerraformFolderToTemp(realTerraformDir, fmt.Sprintf(prefix+"-%s", strings.ToLower(random.UniqueId())))
+	tempTerraformDir, _ := files.CopyTerraformFolderToTemp(realTerraformDir, fmt.Sprintf(prefix+"-%s", strings.ToLower(random.UniqueID())))
 	tags := common.GetTagsFromTravis()
 
 	var region = validRegions[common.CryptoIntn(len(validRegions))]
@@ -271,12 +272,12 @@ func TestRunExistingResourcesInstancesSchematics(t *testing.T) {
 		Upgrade: true,
 	})
 
-	terraform.WorkspaceSelectOrNew(t, existingTerraformOptions, prefix)
-	_, existErr := terraform.InitAndApplyE(t, existingTerraformOptions)
+	terraform.WorkspaceSelectOrNewContext(t, context.Background(), existingTerraformOptions, prefix)
+	_, existErr := terraform.InitAndApplyContextE(t, context.Background(), existingTerraformOptions)
 
 	cloud_logs_existing_en_instances := []map[string]interface{}{
 		{
-			"instance_crn":     terraform.Output(t, existingTerraformOptions, "en_crn_2"),
+			"instance_crn":     terraform.OutputContext(t, context.Background(), existingTerraformOptions, "en_crn_2"),
 			"integration_name": "en-2",
 		},
 	}
@@ -302,7 +303,7 @@ func TestRunExistingResourcesInstancesSchematics(t *testing.T) {
 			TarIncludePatterns: []string{
 				solutionInstanceDADir + "/*.*",
 			},
-			ResourceGroup:          terraform.Output(t, existingTerraformOptions, "resource_group_name"),
+			ResourceGroup:          terraform.OutputContext(t, context.Background(), existingTerraformOptions, "resource_group_name"),
 			TemplateFolder:         solutionInstanceDADir,
 			Tags:                   []string{"test-schematic"},
 			DeleteWorkspaceOnFail:  false,
@@ -317,22 +318,22 @@ func TestRunExistingResourcesInstancesSchematics(t *testing.T) {
 			{Name: "ibmcloud_api_key", Value: options.RequiredEnvironmentVars["TF_VAR_ibmcloud_api_key"], DataType: "string", Secure: true},
 			{Name: "region", Value: region, DataType: "string"},
 			{Name: "cos_region", Value: region, DataType: "string"},
-			{Name: "resource_group_name", Value: terraform.Output(t, existingTerraformOptions, "resource_group_name"), DataType: "string"},
-			{Name: "existing_cloud_logs_data_bucket_crn", Value: terraform.Output(t, existingTerraformOptions, "data_bucket_crn"), DataType: "string"},
-			{Name: "existing_at_cos_target_bucket_name", Value: terraform.Output(t, existingTerraformOptions, "bucket_name_at"), DataType: "string"},
-			{Name: "existing_at_cos_target_bucket_endpoint", Value: terraform.Output(t, existingTerraformOptions, "bucket_endpoint_at"), DataType: "string"},
-			{Name: "existing_cloud_logs_data_bucket_endpoint", Value: terraform.Output(t, existingTerraformOptions, "data_bucket_endpoint"), DataType: "string"},
-			{Name: "existing_cloud_logs_metrics_bucket_crn", Value: terraform.Output(t, existingTerraformOptions, "metrics_bucket_crn"), DataType: "string"},
-			{Name: "existing_cloud_logs_metrics_bucket_endpoint", Value: terraform.Output(t, existingTerraformOptions, "metrics_bucket_endpoint"), DataType: "string"},
-			{Name: "existing_en_instance_crn", Value: terraform.Output(t, existingTerraformOptions, "en_crn_1"), DataType: "string"},
+			{Name: "resource_group_name", Value: terraform.OutputContext(t, context.Background(), existingTerraformOptions, "resource_group_name"), DataType: "string"},
+			{Name: "existing_cloud_logs_data_bucket_crn", Value: terraform.OutputContext(t, context.Background(), existingTerraformOptions, "data_bucket_crn"), DataType: "string"},
+			{Name: "existing_at_cos_target_bucket_name", Value: terraform.OutputContext(t, context.Background(), existingTerraformOptions, "bucket_name_at"), DataType: "string"},
+			{Name: "existing_at_cos_target_bucket_endpoint", Value: terraform.OutputContext(t, context.Background(), existingTerraformOptions, "bucket_endpoint_at"), DataType: "string"},
+			{Name: "existing_cloud_logs_data_bucket_endpoint", Value: terraform.OutputContext(t, context.Background(), existingTerraformOptions, "data_bucket_endpoint"), DataType: "string"},
+			{Name: "existing_cloud_logs_metrics_bucket_crn", Value: terraform.OutputContext(t, context.Background(), existingTerraformOptions, "metrics_bucket_crn"), DataType: "string"},
+			{Name: "existing_cloud_logs_metrics_bucket_endpoint", Value: terraform.OutputContext(t, context.Background(), existingTerraformOptions, "metrics_bucket_endpoint"), DataType: "string"},
+			{Name: "existing_en_instance_crn", Value: terraform.OutputContext(t, context.Background(), existingTerraformOptions, "en_crn_1"), DataType: "string"},
 			{Name: "prefix", Value: options.Prefix, DataType: "string"},
 			{Name: "management_endpoint_type_for_bucket", Value: "direct", DataType: "string"},
 			{Name: "provider_visibility", Value: "public", DataType: "string"},
 			{Name: "enable_platform_metrics", Value: false, DataType: "bool"},
 			{Name: "cloud_logs_existing_en_instances", Value: cloud_logs_existing_en_instances, DataType: "list(object)"},
 			{Name: "cloud_logs_policies", Value: cloud_logs_policies, DataType: "list(object)"},
-			{Name: "existing_cos_instance_crn", Value: terraform.Output(t, existingTerraformOptions, "cos_crn"), DataType: "string"},
-			{Name: "existing_cloud_monitoring_crn", Value: terraform.Output(t, existingTerraformOptions, "cloud_monitoring_crn"), DataType: "string"},
+			{Name: "existing_cos_instance_crn", Value: terraform.OutputContext(t, context.Background(), existingTerraformOptions, "cos_crn"), DataType: "string"},
+			{Name: "existing_cloud_monitoring_crn", Value: terraform.OutputContext(t, context.Background(), existingTerraformOptions, "cloud_monitoring_crn"), DataType: "string"},
 			{Name: "cloud_monitoring_provision", Value: false, DataType: "bool"},
 		}
 
@@ -345,7 +346,7 @@ func TestRunExistingResourcesInstancesSchematics(t *testing.T) {
 			TarIncludePatterns: []string{
 				solutionInstanceDADir + "/*.*",
 			},
-			ResourceGroup:          terraform.Output(t, existingTerraformOptions, "resource_group_name"),
+			ResourceGroup:          terraform.OutputContext(t, context.Background(), existingTerraformOptions, "resource_group_name"),
 			TemplateFolder:         solutionInstanceDADir,
 			Tags:                   []string{"test-schematic"},
 			DeleteWorkspaceOnFail:  false,
@@ -360,9 +361,9 @@ func TestRunExistingResourcesInstancesSchematics(t *testing.T) {
 			{Name: "ibmcloud_api_key", Value: options2.RequiredEnvironmentVars["TF_VAR_ibmcloud_api_key"], DataType: "string", Secure: true},
 			{Name: "cos_region", Value: region, DataType: "string"},
 			{Name: "prefix", Value: options2.Prefix, DataType: "string"},
-			{Name: "resource_group_name", Value: terraform.Output(t, existingTerraformOptions, "resource_group_name"), DataType: "string"},
+			{Name: "resource_group_name", Value: terraform.OutputContext(t, context.Background(), existingTerraformOptions, "resource_group_name"), DataType: "string"},
 			{Name: "kms_endpoint_type", Value: "private", DataType: "string"},
-			{Name: "existing_cos_instance_crn", Value: terraform.Output(t, existingTerraformOptions, "cos_crn"), DataType: "string"},
+			{Name: "existing_cos_instance_crn", Value: terraform.OutputContext(t, context.Background(), existingTerraformOptions, "cos_crn"), DataType: "string"},
 			{Name: "management_endpoint_type_for_bucket", Value: "direct", DataType: "string"},
 			{Name: "enable_platform_metrics", Value: false, DataType: "bool"},
 			{Name: "existing_kms_instance_crn", Value: permanentResources["hpcs_south_crn"], DataType: "string"},
@@ -380,8 +381,8 @@ func TestRunExistingResourcesInstancesSchematics(t *testing.T) {
 		fmt.Println("Terratest failed. Debug the test and delete resources manually.")
 	} else {
 		logger.Log(t, "START: Destroy (existing resources)")
-		terraform.Destroy(t, existingTerraformOptions)
-		terraform.WorkspaceDelete(t, existingTerraformOptions, prefix)
+		terraform.DestroyContext(t, context.Background(), existingTerraformOptions)
+		terraform.WorkspaceDeleteContext(t, context.Background(), existingTerraformOptions, prefix)
 		logger.Log(t, "END: Destroy (existing resources)")
 	}
 }
