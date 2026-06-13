e1544c900cd8866bafae09c7bff92d133751b6ef
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-06-13 12:27:46 +0000

    fix(deps): update dependencies

--- solutions/agents/version.tf
+++ solutions/agents/version.tf
@@ -6,15 +6,15 @@ terraform {
     # Lock DA into an exact provider version - renovate automation will keep it updated
     ibm = {
       source  = "ibm-cloud/ibm"
-      version = "1.89.0"
+      version = "2.2.2"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "3.1.1"
+      version = "3.2.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "3.0.1"
+      version = "3.2.0"
     }
   }
 }
