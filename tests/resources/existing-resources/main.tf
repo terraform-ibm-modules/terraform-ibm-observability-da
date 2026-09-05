41cb7399458898818309398d1d7afea6d1caf1d9
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-09-05 20:42:44 +0530

    fix(deps): update dependencies (#616)

--- tests/resources/existing-resources/main.tf
+++ tests/resources/existing-resources/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "cos" {
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "10.17.9"
+  version                = "10.17.10"
   resource_group_id      = module.resource_group.resource_group_id
   region                 = var.region
   cos_instance_name      = "${var.prefix}-cos"
@@ -27,7 +27,7 @@ module "cos" {
 
 module "additional_cos_bucket" {
   source                   = "terraform-ibm-modules/cos/ibm"
-  version                  = "10.17.9"
+  version                  = "10.17.10"
   region                   = var.region
   create_cos_instance      = false
   existing_cos_instance_id = module.cos.cos_instance_id
@@ -37,7 +37,7 @@ module "additional_cos_bucket" {
 
 module "cloud_log_buckets" {
   source  = "terraform-ibm-modules/cos/ibm//modules/buckets"
-  version = "10.17.9"
+  version = "10.17.10"
   bucket_configs = [
     {
       bucket_name            = "${var.prefix}-data-bucket"
